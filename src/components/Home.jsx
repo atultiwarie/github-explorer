0ee434d5c031268ae49e1ec7f4ef5ed30a707416
atultiwarie <atultiwari4767@gmail.com>
2025-08-20 23:26:43 +0530

    updated home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,13 @@ const Home = () => {
   const [user, setUser] = useState(null); // store user object
 
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center">
       <h1 className="text-3xl font-bold text-center py-5">GitHub Explorer</h1>
 
-      {/* SearchUser now informs Home.jsx about the user */}
-      <SearchUser onSearch={setUser} />
+      {/* Centered SearchUser */}
+      <div className="w-full flex justify-center mb-5">
+        <SearchUser onSearch={setUser} />
+      </div>
 
       {/* Display repos if user exists */}
       {user && <UserRepos username={user.login} />}
